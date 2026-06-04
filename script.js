6492286f5a211ca969807232834cbfed24e0316a
Thanh To <thanhyto.per@gmail.com>
2026-06-04 12:49:34 -0700

    make words go up

--- script.js
+++ script.js
@@ -23,41 +23,48 @@ textarea.addEventListener("input", (event) => {
         const wordColor = randomHSL();
         const xPos = Math.random() * 0.9 * window.innerWidth;
         const yPos = (Math.random() * 0.5 + 0.4) * window.innerHeight;
-        allWords.push({...wordBubbleObject, word: lastWord, size: wordSize, color: wordColor, xPos: xPos, yPos: yPos});
+        const wordBubble = {
+          word: lastWord,
+          size: wordSize,
+          color: wordColor,
+          xPos: xPos,
+          yPos: yPos,
+          // opacity: Math.random() * 0.5 + 0.5
+        }
+        allWords.push(wordBubble);
         // console.log("all words:", words);
-        // console.log("global all words:", allWords);
+        console.log("global all words:", allWords);
         console.log("word released:", lastWord);
-        
-        launchWord(lastWord, wordSize, wordColor, xPos, yPos);
+        launchWord(wordBubble);
     }
 });
 // Animation loop
 let start; // Start time for animation 
 // Bubble animation
-function launchWord(word, size, color, xPos, yPos) {
+function launchWord(wordBubble) {
   
   // Appear at the bottom half of the screen
   const bubble = document.createElement('span');
-  bubble.textContent = word;
+  bubble.textContent = wordBubble.word;
   bubble.style.position = 'absolute';
-  bubble.style.fontSize = `${size}px`;
-  bubble.style.color = color;
-  bubble.style.left = `${xPos}px`;
-  bubble.style.top = `${yPos}px`;
+  bubble.style.fontSize = `${wordBubble.size}px`;
+  bubble.style.color = wordBubble.color;
+  bubble.style.left = `${wordBubble.xPos}px`;
+  bubble.style.top = `${wordBubble.yPos}px`;
+  wordBubble.bubble = bubble; // Store the bubble element in the wordBubble object
   console.log(bubble);
   // Update allWords with the new bubble
-  allWords.push()
   document.getElementById('stage').appendChild(bubble);
 }
 // Animation function 
 function animate(timestamp) {
   if (start === undefined) {
-    start = timestamp
-
+    start = timestamp;
   }
   const elapsed = timestamp - start;
   for(let i = 0; i < allWords.length; i++){
-    allWords[i].yPos -= 0.001 * elapsed;
+    allWords[i].yPos -= 0.5;
+    allWords[i].bubble.style.top = `${allWords[i].yPos}px`;
   }
   requestAnimationFrame(animate);
 }
